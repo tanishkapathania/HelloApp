4204b91967c513d68c45e15114d03e6f31803e12
Tanishka <mail2tanishkapathania@gmail.com>
2026-03-18 15:08:26 +0530

    Code for UC3

--- HelloApp.java
+++ HelloApp.java
@@ -1,8 +1,9 @@
-public class HelloApp{
- public static void main(String[] args){
-    if(args.length>0){
-      String name = args[0];
-      System.out.println("Hello,"+name + "!");
-}
-}
-}
+public class HelloApp {
+    public static void main(String[] args) {
+        String name = "World";
+        if (args.length > 0) {
+            name = args[0]; 
+        }
+        System.out.println("Hello, " + name + "!");
+      }
+      }

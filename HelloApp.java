0ed1927c4d12499b73f9c2c8602a67105364b778
Tanishka <mail2tanishkapathania@gmail.com>
2026-03-18 14:44:17 +0530

    Code for UC2

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,8 @@
 public class HelloApp{
-    public static void main (String[] args){
-         System.out.println("Hello World!");
-    }
+ public static void main(String[] args){
+    if(args.length>0){
+      String name = args[0];
+      System.out.println("Hello,"+name + "!");
+}
+}
 }

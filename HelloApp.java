b97493a19293c37c5ff8ca5e261ed5dec1ebbc58
Tanishka <mail2tanishkapathania@gmail.com>
2026-03-29 18:06:52 +0530

    Code for UC6

--- HelloApp.java
+++ HelloApp.java
@@ -1,20 +1,25 @@
+
 public class HelloApp {
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            for (String name : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(name);
-                first = false;
-            }
-
-            System.out.println("Hello, " + nameBuilder.toString() + "!");
+            return;
         }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        for (String name : args) {
+           
+            nameBuilder.append(name).append(", ");
+        }
+
+        String finalNames = "";
+
+        if (nameBuilder.length() > 0) {
+           
+            finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
+        }
+
+             System.out.println("Hello, " + finalNames + "!");
     }
 }
